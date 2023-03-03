a5056c86103118aef04412920d708249df4e9060
wagblin3 <wagblin3@gmail.com>
2023-03-03 09:39:35 +0100

    env var : test 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
     apiKey_apiTmdb: '267af5120325d394d4ce784b430c2043',
     base_url_apiBack: 'https://redline.fr.nf/api',
     // base_url_apiBack: 'http://localhost:8080/api',
-    url_apiUser: 'http://localhost:8082/login',
+    url_apiUser: 'https://redline.fr.nf/login',
+    // url_apiUser: 'http://localhost:8082/login',
 };
