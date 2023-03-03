e86052b15397e74e71c4e81dd0fa8fe975663300
wagblin3 <wagblin3@gmail.com>
2023-03-03 10:15:01 +0100

    supression de environment.staging

--- src/environments/environment.staging.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    base_url_apiTmdb : 'https://api.themoviedb.org/3',
-    apiKey_apiTmdb: '267af5120325d394d4ce784b430c2043',
-    base_url_apiBack: 'https://redline.fr.nf/api',
-    // base_url_apiBack: 'http://localhost:8080/api',
-    url_apiUser: 'http://localhost:8082/login',
-};
